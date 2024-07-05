6ab16f48007a664097b8cf435c9f69e1b104f88b
mr0-0kushal <kushalsharmahacker678@gmail.com>
2024-07-05 17:29:40 +0530

    Fourth Commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,8 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/free-mode'
-import { FreeMode, Pagination } from 'swiper/modules' 
+import 'swiper/css/scrollbar'
+import { FreeMode, Pagination, Scrollbar, Mousewheel } from 'swiper/modules'
 
 const Slider = () => {
 
@@ -10,35 +11,37 @@ const Slider = () => {
         {
             title: "Lorem Ipsum",
             content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero fugiat facilis aliquid voluptatibus sit dolores ad beatae nesciunt itaque vel?",
-            backgroundImage: "/images/cardBgmobile.png",
+            backgroundImage: "/images/dummy.jpg",
         },
         {
             title: "Lorem Ipsum",
             content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero fugiat facilis aliquid voluptatibus sit dolores ad beatae nesciunt itaque vel?",
-            backgroundImage: "/images/cardBgmobile.png",
+            backgroundImage: "/images/dummy.jpg",
         },
         {
             title: "Lorem Ipsum",
             content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero fugiat facilis aliquid voluptatibus sit dolores ad beatae nesciunt itaque vel?",
-            backgroundImage: "/images/cardBgmobile.png",
+            backgroundImage: "/images/dummy.jpg",
         }, {
             title: "Lorem Ipsum",
             content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero fugiat facilis aliquid voluptatibus sit dolores ad beatae nesciunt itaque vel?",
-            backgroundImage:"/images/cardBgmobile.png",
+            backgroundImage: "/images/dummy.jpg",
         }, {
             title: "Lorem Ipsum",
             content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero fugiat facilis aliquid voluptatibus sit dolores ad beatae nesciunt itaque vel?",
-            backgroundImage: "/images/cardBgmobile.png",
+            backgroundImage: "/images/dummy.jpg",
         },
         {
             title: "Lorem Ipsum",
             content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero fugiat facilis aliquid voluptatibus sit dolores ad beatae nesciunt itaque vel?",
-            backgroundImage: "/images/cardBgmobile.png",
+            backgroundImage: "/images/dummy.jpg",
         },
     ];
 
     return (
-        <div className="slideBar flex items-center justify-center flex-col min-h-60 p-3 lg:p-6 bg-transparent">
+        <div className="slideBar flex items-center justify-center gap-6 flex-col min-h-60 p-3 lg:p-6 bg-cover bg-center bg-no-repeat bg-fixed" style={{ backgroundImage: 'url(/images/cardBgmobile.png)' }}>
+            <h1 className="headingText text-4xl font-bold text-center">Our CSR</h1>
+            <h3 className="subHeadingText text-wrap w-3/4 text-center text-xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum perspiciatis architecto, excepturi quas delectus tempore laboriosam quaerat harum magnam dignissimos?</h3>
             <Swiper
                 breakpoints={{
                     340: {
@@ -50,23 +53,23 @@ const Slider = () => {
                         spaceBetween: 10
                     }
                 }}
+                modules={[FreeMode, Pagination, Scrollbar, Mousewheel]}
 
                 freeMode={true}
-                Pagination={{
-                    clickable: true
-                }}
-                modules={[FreeMode, Pagination]}
+                Pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                mousewheel={{ forceToAxis: true }}
                 className='max-w-[90%] lg:max-w[80%]'>
 
                 {ServiceData.map((item) => (
                     <SwiperSlide key={item.title} className='p-4'>
-                        <div className="flex flex-col gap-6 group relative shadow-md shadow-black rounded-xl px-5 py-8 h-[280px] w-[170px] lg:h-[400px] lg:w-[310px] overflow-hidden">
-                            <div className="absolute inset-0 bg-cover bg-center" style={{backgroundImage:`url(${item.backgroundImage})`}} />
-                            <div className="absolute inset-0 bg-[#FBBDBB] opacity-10 hover:opacity-50" />
-                            <div className="relative flex flex-col gap-3">
-                                <h1 className='text-xl lg:text-2xl font-bold'>{item.title}</h1>
-                                <p className="text-sm lg:text-[18px]">{item.content}</p>
-                           </div>
+                        <div className="flex flex-col gap-6 group relative shadow-md shadow-black rounded-xl h-[280px] w-[170px] lg:h-[400px] lg:w-[310px] overflow-hidden">
+                            <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${item.backgroundImage})` }} />
+                            <div className="relative flex flex-col gap-3 h-full">
+                            <div className="absolute inset-0 bg-[#FBBDBB] opacity-0 hover:opacity-50 transition-all duration-200" />
+                                <h1 className='text-xl lg:text-2xl font-extrabold text-shadow-md-white p-3 lg:px-5 lg:py-8 z-10'>{item.title}</h1>
+                                <div className="text-sm lg:text-[18px] absolute w-full bottom-0 bg-white bg-opacity-70 p-3 lg:px-5 lg:py-8 z-10">{item.content}</div>
+                            </div>
                         </div>
                     </SwiperSlide>
                 ))}
