86e3f72f1d63651da1837ecf2212481c7b173bbc
mr0-0kushal <kushalsharmahacker678@gmail.com>
2024-07-07 14:58:11 +0530

    Sixth Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 import Header from "./components/Header"
-import DivueensCSR from "./components/DivueensCSR"
+import DivueensCSR from "./components/CareerCSR/CareerCSR"
 import Footer from "./components/Footer"
 import './input.css'
 
-import './App.css'
+import '../src/components/CareerCSR/CareerCSR.css'
 
 
 function App() {
